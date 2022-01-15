9feb42a61d434f41c603e6912025ac9b6ac254c8
Luis Torres Cereño <torrescereno@gmail.com>
2022-01-15 15:41:01 -0300

    tercer ejercicio

---
+++ app_3.py
@@ -0,0 +1,35 @@
+"""
+Realiza una función llamada relacion() que a partir de dos números cumpla lo siguiente:
+
+1. Si el primer número es mayor que el segundo, debe devolver 1.
+2. Si el primer número es menor que el segundo, debe devolver -1.
+3. Si ambos números son iguales, debe devolver un 0.
+
+Comprueba la relación entre los números: '5 y 10', '10 y 5' y '5 y 5'
+"""
+
+from utilities import check_system, clear_console
+
+
+def relacion(a, b):
+    if a > b:
+        return 1
+    if a < b:
+        return -1
+    if a == b:
+        return 0
+
+
+if __name__ == "__main__":
+    command = check_system()
+    try:
+        while True:
+            print("Calculo del área de un círculo")
+            a = int(input("Ingrese el primer número: "))
+            b = int(input("Ingrese el segundo número: "))
+            result = relacion(a, b)
+            print(f"El resultado es: {result}")
+            break
+    except Exception:
+        clear_console(command)
+        print("Debe ingresar un número")
