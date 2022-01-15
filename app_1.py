dddfff3e5930e12049af4522cbb8fb67089653d1
Luis Torres Cereño <torrescereno@gmail.com>
2022-01-15 15:25:42 -0300

    primer ejercicio

---
+++ app_1.py
@@ -0,0 +1,26 @@
+"""
+Realiza una función llamada area_rectangulo()
+que devuelva el área del rectángulo a partir de una base y una altura.
+Calcula el área de un rectángulo de 15 de base y 10 de altura
+"""
+
+from utilities import check_system, clear_console, print_menu
+
+
+def area_rectangulo(l, b):
+    return l * b
+
+
+if __name__ == "__main__":
+    command = check_system()
+    try:
+        while True:
+            print("Calculo del area de un rectangulo")
+            l = int(input("Ingrese la altura "))
+            b = int(input("Ingrese la base "))
+            area = area_rectangulo(l, b)
+            print(f"El area del rectangulo es: {area}")
+            break
+    except Exception:
+        clear_console(command)
+        print("Debe ingresar un número")
